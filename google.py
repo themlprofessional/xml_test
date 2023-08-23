65e4f0e9cd7edf57176a3adc62c02966b60f762d
themlprofessional <117509273+themlprofessional@users.noreply.github.com>
2023-08-23 15:16:54 +0530

    Update google.py

--- google.py
+++ google.py
@@ -1,10 +1,17 @@
+# Import SparkSession
+from pyspark.sql import SparkSession
+
 # Create a SparkSession
-spark = SparkSession.builder.appName("WriteDataFrameToGCS").getOrCreate()
+spark = SparkSession.builder.appName("Spark XML Example").getOrCreate()
 
 # Read the input data
-df = spark.read.csv("input_data.csv")
+df = spark.read.csv("input.csv")
 
-# Convert the Spark DataFrame to an XML file
-df_write = df.toDF("name", "age")
+# Convert the Spark dataframe to an XML file
+df_write = df.write.format("xml").save("output.xml")
+
+# Convert the XML file to a compressed XML file
 df_write = df_write.option("codec", "org.apache.hadoop.io.compress.GzipCodec")
-df_write.write.format("xml").save("gs://bucket_name/output_data.xml.gz")
+
+# Upload the compressed XML file to a GCS bucket
+df_write.save("gs://my-bucket/my-file.xml.gz")
